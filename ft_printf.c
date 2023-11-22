6872e45699805e1038b5f874d3ff9a4611c40886
stephanocastillo <iamaxlstephano@gmail.com>
2023-11-22 16:32:40 +0100

    221023

--- ft_printf.c
+++ ft_printf.c
@@ -6,45 +6,55 @@
 /*   By: axcastil <axcastil@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/13 16:41:16 by axcastil          #+#    #+#             */
-/*   Updated: 2023/11/21 13:11:31 by axcastil         ###   ########.fr       */
+/*   Updated: 2023/11/22 16:30:57 by axcastil         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "printf.h"
+#include "ft_printf.h"
 #include <unistd.h>
 
-void	*inspect(char *letter)
+void	*inspect(va_list args, char *letter, int *number)
 {
-	letter = 'b';
-/*		if (*letter == 'c')
+	if (*letter != '\0')			
+	{
+		if (*letter == 's')
 			
-		else if (*letter == 's')
 		else if (*letter == 'p')
 		else if (*letter == 'd')
 		else if (*letter == 'i')
 		else if (*letter == 'u')
 		else if (*letter == 'x')
 		else if (*letter == 'X')
-		else (*letter == '%')	*/
+		else if	(*letter == '%')
+		else
+			ft_putchar_print(*letter, *number);
+	}
+	else
+		ft_putchar_print()
 }
 
-int	ft_printf(char const *sentence)
+int	ft_printf(char const *sentence, ...)
 {
-/*	va_list args; 
-	va_start(args, sentence);*/
+	va_list	args;
+	size_t	j;
+	
+	if (!sentence)
+		return (0);
+	j = 0;
+	va_start(args, sentence);
 	while (*sentence)
 	{
 		if (*sentence == '%')
 		{
-			inspect();
 			sentence++;
+			inspect(args, &(*(sentence + 1)), &j);
 		}
 		else
-			write(1, sentence, 1);
+			ft_putchar_print(*sentence, &j);
 		sentence++;
 	}
-/*	va_end(args);
-*/	return (1);
+	va_end(args);
+	return (j);
 }
 
 int main()
--- ft_putchar_printf.c
+++ ft_putchar_printf.c
@@ -1,25 +1,19 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_printf.h                                        :+:      :+:    :+:   */
+/*   ft_putchar_printf.c                                :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: axcastil <axcastil@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/11/19 18:08:05 by axcastil          #+#    #+#             */
-/*   Updated: 2023/11/22 15:22:01 by axcastil         ###   ########.fr       */
+/*   Created: 2023/11/22 15:18:04 by axcastil          #+#    #+#             */
+/*   Updated: 2023/11/22 15:20:58 by axcastil         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef FT_PRINTF_H
-# define FT_PRINTF_H
+#include "ft_printf.h"
 
-void	ft_putchar_print(char c, int *number);
-
-# include <unistd.h>
-# include <string.h>
-# include <stdlib.h>
-# include <stdio.h>
-# include <fcntl.h>
-# include <stdarg.h>
-
-#endif
+void	ft_putchar_print(char c, int *number)
+{
+	write(1, &c, 1);
+	*number++;
+}
